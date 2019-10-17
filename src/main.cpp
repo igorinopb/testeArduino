32fad3784071c97896a5532738c830f8e111716b
igor <igorportugalbazilio@gmail.com>
2019-10-17 04:11:31 -0300

    Upload de todos arquivos

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+void setup() {
+
+  // // Teste 1 - Definindo pinos de saída
+  // pinMode(4, OUTPUT);
+  // pinMode(5, OUTPUT);
+  // pinMode(8, OUTPUT);
+  // pinMode(9, OUTPUT);
+  // pinMode(13, OUTPUT);
+
+  // // Teste 1 - Definindo pinos como ground
+  // digitalWrite(5, LOW);
+  // digitalWrite(9, LOW);
+
+  // // Teste 2 - Iniciando Serial a 9600 bps
+  // Serial.begin(9600);
+}
+
+void loop() {
+
+  // // Teste 1 - Fazendo 3 LEDs piscarem sequencialmente
+  // digitalWrite(13, HIGH); delay(100);
+  // digitalWrite(13, LOW); delay(100);
+
+  // digitalWrite(8, HIGH); delay(100);
+  // digitalWrite(8, LOW); delay(100);
+
+  // digitalWrite(4, HIGH); delay(100);
+  // digitalWrite(4, LOW); delay(100);
+
+  // // Teste 2 - Escrevendo "Olá Mundo!" no Serial
+  // Serial.print("Hello World!");
+}
